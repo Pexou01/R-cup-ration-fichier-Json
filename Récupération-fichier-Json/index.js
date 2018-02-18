e0603067917001e8d556ad599802e4be6293aa1b
frederic dupont <pexou01@gmail.com>
2018-02-18 18:33:43 +0100

    Json

---
+++ Récupération-fichier-Json/index.js
@@ -0,0 +1,25 @@
+const main = document.querySelector('#main');
+const url = 'https://thejsway-server.herokuapp.com/api/articles';
+
+fetch(url)
+.then(result => result.json())
+.then(json => {
+  json.forEach(({ id, title, content}) =>{
+  const div =  document.createElement('div');
+  const h2 =  document.createElement('h2');
+  const p =  document.createElement('p');
+
+  const nTitle = document.createTextNode(`${id} - ${title}`);
+  const nContent = document.createTextNode(content);
+
+  main.appendChild(div);
+  div.appendChild(h2);
+  div.appendChild(p);
+
+  h2.appendChild(nTitle);
+  p.appendChild(nContent);
+  })
+})
+.catch(err => {
+  console.error(err);
+});
